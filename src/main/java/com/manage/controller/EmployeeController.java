213f9c0ae5eabef8973444e7d9f48844a12598e1
Minh Tuan <tranminhtuannhj@gmail.com>
2023-04-04 14:48:01 +0700

    Commit

--- src/main/java/com/manage/controller/EmployeeController.java
+++ src/main/java/com/manage/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.manage.dto.EmployeeDTO;
 import com.manage.jsonview.EmployeeViews;
 import com.manage.services.bl.GetEmployeeInforService;
 import com.manage.services.bl.RegisterEmployeeService;
+import com.manage.services.bl.UpdateStatusForEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +30,9 @@ public class EmployeeController {
   @Autowired
   private RegisterEmployeeService registerEmployeeService;
 
+  @Autowired
+  private UpdateStatusForEmployeeService updateStatusForEmployeeService;
+
   @GetMapping("/all-employee/{status}")
   @JsonView({EmployeeViews.EmployeeViewSet.class})
   @PreAuthorize("hasRole('ADMIN')")
@@ -41,6 +45,14 @@ public class EmployeeController {
   @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<?> registerEmployee(@RequestBody EmployeeDTO employeeDTO) throws SystemException {
     Boolean result = registerEmployeeService.registerEmployee(employeeDTO);
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  @PostMapping("/update-status/{employeeId}/{status}")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<?> updateStatusForEmployee(@PathVariable(value = "employeeId") Long employeeId,
+                                                   @PathVariable(value = "status") Integer status) throws SystemException {
+    Boolean result = updateStatusForEmployeeService.updateStatusForEmployee(employeeId, status);
     return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 
---
+++ src/main/java/com/manage/services/bl/UpdateStatusForEmployeeService.java
@@ -0,0 +1,58 @@
+package com.manage.services.bl;
+
+import com.manage.model.Employee;
+import com.manage.model.WorkHistory;
+import com.manage.repository.EmployeeRepository;
+import com.manage.repository.WorkHistoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.SystemException;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UpdateStatusForEmployeeService {
+
+  private static final Logger logger = LoggerFactory.getLogger(UpdateStatusForEmployeeService.class);
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @Autowired
+  private WorkHistoryRepository workHistoryRepository;
+
+  public Boolean updateStatusForEmployee(Long employeeId, Integer status) throws SystemException {
+    try {
+      updateStatus(employeeId, status);
+      updateWorkHistory(employeeId);
+      return true;
+    } catch (Exception e) {
+      logger.error("Error", e);
+      throw new SystemException();
+    }
+  }
+
+  private void updateStatus(Long employeeId, Integer status) {
+    Employee employee = employeeRepository.findEmployeeByEmployeeId(employeeId);
+    employee.setStatus(status);
+    employee.setModifiedDate(new Date());
+    employee.setModifiedBy("Admin");
+    employeeRepository.save(employee);
+  }
+
+  private void updateWorkHistory(Long employeeId) {
+    List<WorkHistory> workHistoryList = workHistoryRepository.getWorkHistoryByEmployeeId(employeeId);
+    if (Objects.nonNull(workHistoryList) && !workHistoryList.isEmpty()) {
+      WorkHistory workHistory = workHistoryList.get(workHistoryList.size() - 1);
+      workHistory.setEndDate(new Date());
+      workHistory.setModifiedDate(new Date());
+      workHistory.setModifiedBy("Admin");
+      workHistoryRepository.save(workHistory);
+    }
+  }
+
+}
