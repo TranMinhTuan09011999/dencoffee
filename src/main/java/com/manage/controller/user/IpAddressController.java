9bb60455ebcd222f66248ff1b4f56b90ca5b901e
Minh Tuan <tranminhtuannhj@gmail.com>
2023-04-10 17:01:47 +0700

    Commit

--- src/main/java/com/manage/controller/user/IpAddressController.java
+++ src/main/java/com/manage/controller/user/IpAddressController.java
@@ -1,9 +1,12 @@
 package com.manage.controller.user;
 
 import com.manage.services.bl.CheckIpAddressService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +20,42 @@ import java.net.UnknownHostException;
 @RequestMapping("/api/user/ip")
 public class IpAddressController {
 
+  private static final Logger logger = LoggerFactory.getLogger(IpAddressController.class);
+
   @Autowired
   private CheckIpAddressService checkIpAddressService;
 
   @GetMapping("/ip-address-attendance")
   public ResponseEntity<?> getIpAddressForAttedance(HttpServletRequest request) throws SystemException, UnknownHostException {
+    String remoteAddr = getIpAdressByHeader(request);
+    Boolean result = checkIpAddressService.checkIpAddress(remoteAddr);
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  public static String getIpAddress(HttpServletRequest request) throws UnknownHostException {
     String remoteAddr = request.getRemoteAddr();
     if (remoteAddr.equals("0:0:0:0:0:0:0:1")) {
       InetAddress localip = InetAddress.getLocalHost();
       remoteAddr = localip.getHostAddress();
     }
-    Boolean result = checkIpAddressService.checkIpAddress(remoteAddr);
-    return ResponseEntity.status(HttpStatus.OK).body(result);
+
+    logger.info("After process getRemoteAddr: {}", remoteAddr);
+    return remoteAddr;
+  }
+
+  public static String getIpAdressByHeader(HttpServletRequest request) throws UnknownHostException {
+    logger.info("getIpAdressByHeader - START");
+    String ip = request.getHeader("X-Forwarded-For");
+    if(!StringUtils.isEmpty(ip)) {
+      logger.info("IP Access from X-Forwarded-For: " + ip);
+      logger.info("getIpAdressByHeader - END");
+      return ip;
+    } else {
+      logger.info("Before process getRemoteAddr: {}", request.getRemoteAddr());
+      ip = getIpAddress(request);
+      logger.info("After process getRemoteAddr: {}", ip);
+      logger.info("getIpAdressByHeader - END");
+      return ip;
+    }
   }
 }
